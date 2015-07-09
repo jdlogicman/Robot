7e39d7e34a230e96d236a63c6d47ab519e0e2955
John Duddy <jduddy@gmail.com>
2015-07-09 16:36:11 -0700

    Stuff

---
+++ Robot/BouyancySystem.cs
@@ -0,0 +1,23 @@
+﻿using ControlLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    public class BouyancySystem : ObserverBase
+    {
+        public const double MAX_LOAD_KG = 5;
+
+        public double WaterMass { get; private set; }
+
+        
+        public override void OnNext(double amount)
+        {
+            WaterMass = Math.Min(Math.Max(0, WaterMass + amount), MAX_LOAD_KG);
+            base.OnNext(WaterMass);
+        }
+    }
+}
---
+++ Simulation/PressureSimulation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation
+{
+    public class PressureSimulation
+    {
+        public const double BAR_PER_M = 0.1;
+        public static double CalculatePressure(double velocityNow, double pressureNow, uint deltaTInMilliSeconds = 1000)
+        {
+            return Math.Max(0, pressureNow + velocityNow * (deltaTInMilliSeconds / 1000.0) * BAR_PER_M);
+
+        }
+    }
+}
